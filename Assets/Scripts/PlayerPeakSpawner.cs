32a553f4d247c2f33a5cc23cd0d1ddf6ca3e08bf
Dacarpe03 <d.carmonap97@gmail.com>
2022-05-26 16:21:09 +0200

    Add code to spawn with offset

--- Assets/Scripts/PlayerPeakSpawner.cs
+++ Assets/Scripts/PlayerPeakSpawner.cs
@@ -5,11 +5,14 @@ public class PlayerPeakSpawner : MonoBehaviour
     // Update is called once per frame
     [SerializeField] GameObject pickaxe;
     private KeyCode lastPressedKey;
-    private float xSpawnOffset = 0;
-    private float ySpawnOffset = 0;
+    [SerializeField] float xSpawnOffset = 20f;
+    private float xMultiplier = 0f;
+    [SerializeField] float ySpawnOffset = 20f;
+    private float yMultiplier = 0f;
     
     void Update()
     {
+        UpdateSpawningoffsets();
         ProcessPickaxe();
     }
 
@@ -20,10 +23,30 @@ public class PlayerPeakSpawner : MonoBehaviour
     }
 
     private void SpawnPickaxe(){
-        Instantiate(pickaxe, transform.position, Quaternion.identity);
+        Vector3 spawnPosition = transform.position + new Vector3(xMultiplier*xSpawnOffset, yMultiplier*ySpawnOffset, 0f);
+        Instantiate(pickaxe, spawnPosition, Quaternion.identity);
     }
 
-    private void UpdateLastDirection(){
+    private void UpdateSpawningoffsets(){
+        if (Input.GetKeyDown(KeyCode.A)){
+            xMultiplier = 1;
+            yMultiplier = 0; 
+        }
 
+        if (Input.GetKeyDown(KeyCode.D)){
+            xMultiplier = -1;
+            yMultiplier = 0; 
+        }
+
+        
+        if (Input.GetKeyDown(KeyCode.W)){
+            xMultiplier = 0;
+            yMultiplier = 1; 
+        }
+        
+        if (Input.GetKeyDown(KeyCode.S)){
+            xMultiplier = 0;
+            yMultiplier = -1; 
+        }
     }
 }
