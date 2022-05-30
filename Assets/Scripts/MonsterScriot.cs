ce13a9decc3eb15a62983b05f0796ea12614e962
Dacarpe03 <d.carmonap97@gmail.com>
2022-05-30 16:49:51 +0200

    More sounds

--- Assets/Scripts/MonsterScriot.cs
+++ Assets/Scripts/MonsterScriot.cs
@@ -8,16 +8,20 @@ public class MonsterScriot : MonoBehaviour
     [SerializeField] float speed = 4f;
     [SerializeField] float activateDistance = 3f;
     [SerializeField] GameObject deadMonster;
+    private AudioSource shoutSource;
+
     private Vector3 startPosition;
     private bool follow = false;
     private Rigidbody2D rb;
     private bool stopFollowingCalled = false;
+    private bool soundMade = false;
 
     private float nextTime;
     private float wait = 0.5f;
     // Start is called before the first frame update
     void Start()
     {
+        shoutSource = this.GetComponent<AudioSource>();
         rb = this.gameObject.GetComponent<Rigidbody2D>();
         startPosition = rb.position;
         playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
@@ -33,6 +37,11 @@ public class MonsterScriot : MonoBehaviour
             }
 
             if (follow){
+                if (!soundMade){
+                    Debug.Log("sound");
+                    soundMade = true;
+                    shoutSource.Play();
+                }
                 transform.position = Vector2.MoveTowards(rb.position, playerPosition.position, speed*Time.deltaTime);
             }
 
@@ -50,6 +59,7 @@ public class MonsterScriot : MonoBehaviour
     public void StopFollowing(){
         stopFollowingCalled = true;
         nextTime = Time.time + wait;
+        soundMade = false;
         this.follow = false;
         rb.position = startPosition;
         transform.position = startPosition;
